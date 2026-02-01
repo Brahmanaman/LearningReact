69d7426d474f633a0d0db72a4b485a64588eb50d
Aman Sharma <sharma9917375201@gmail.com>
2026-02-01 22:13:36 +0530

    added routes

---
+++ 26-TanStackQuery(ReactQuery)/src/layout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router";
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
+
+const MainLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default MainLayout;
---
+++ 26-TanStackQuery(ReactQuery)/src/pages/FetchQR.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FetchQR = () => {
+  return (
+    <div>FetchQR</div>
+  )
+}
+
+export default FetchQR
