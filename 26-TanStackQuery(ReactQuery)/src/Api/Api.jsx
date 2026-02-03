9496fad71ed5e7b43d0257bfc138350dff18a188
Aman Sharma <sharma9917375201@gmail.com>
2026-02-03 16:34:11 +0530

    added pagination

--- 26-TanStackQuery(ReactQuery)/src/Api/Api.jsx
+++ 26-TanStackQuery(ReactQuery)/src/Api/Api.jsx
@@ -4,12 +4,12 @@ const api = axios.create({
   baseURL: "https://jsonplaceholder.typicode.com",
 });
 
-export const fetchPost = async () => {
-  const result = await api.get("/posts");
+export const fetchPost = async (pageNumber) => {
+  const result = await api.get(`/posts?_start=${pageNumber}&_limit=10`);
   return result.status == 200 ? result.data : [];
 };
 
-export const fetchIndividualData = async (id) =>{
+export const fetchIndividualData = async (id) => {
   const result = await api.get(`/posts/${id}`);
   return result.status == 200 ? result.data : [];
-}
+};
--- 26-TanStackQuery(ReactQuery)/src/pages/FetchQR.jsx
+++ 26-TanStackQuery(ReactQuery)/src/pages/FetchQR.jsx
@@ -1,12 +1,15 @@
 import { fetchPost } from "../Api/Api";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { NavLink } from "react-router";
 
 const FetchQR = () => {
+  const [pageNumber, setPageNumber] = useState(0);
   // Queries
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ["posts"],
-    queryFn: fetchPost,
+    queryKey: ["posts", pageNumber],
+    queryFn: () => fetchPost(pageNumber),
+    placeholderData: keepPreviousData,
     // gcTime: 10000,
     // staleTime: 20000,
     // refetchInterval: 1000,
@@ -17,7 +20,7 @@ const FetchQR = () => {
   if (isError) return <p>Error : {error.message || "something went wrong"}</p>;
   return (
     <>
-      <div>
+      <div className="post-container">
         {data?.map((post) => {
           return (
             <div key={post.id} className="post">
@@ -28,6 +31,15 @@ const FetchQR = () => {
             </div>
           );
         })}
+        <div className="dvbtn">
+          <button onClick={() => setPageNumber((prev) => prev - 10)}>
+            Prev
+          </button>
+          <h2>{pageNumber / 10 + 1}</h2>
+          <button onClick={() => setPageNumber((prev) => prev + 10)}>
+            Next
+          </button>
+        </div>
       </div>
     </>
   );
