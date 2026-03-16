0567ed725ccd02ded7af8a91a4ae0347c119498d
Aman Sharma <sharma9917375201@gmail.com>
2026-03-16 23:57:04 +0530

    form handling

--- 09-forms-handling/src/App.jsx
+++ 09-forms-handling/src/App.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import { useState } from "react";
 
 const App = () => {
+  let [formValue, setFormValue] = useState({});
+
+  function formHandle(e) {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+  }
   function submitHandler(e) {
     e.preventDefault();
-    console.log("submit handler");
+    console.log(formValue);
+    setFormValue({
+      name: "",
+      email: "",
+      password: "",
+    });
   }
 
   return (
@@ -13,7 +23,39 @@ const App = () => {
           submitHandler(e);
         }}
       >
-        <input type="text" name="" id="" placeholder="Enter your name" />
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={formValue.name}
+            placeholder="Enter your name"
+            onChange={formHandle}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            value={formValue.email}
+            placeholder="Enter your email"
+            onChange={formHandle}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            value={formValue.password}
+            placeholder="Enter your password"
+            onChange={formHandle}
+          />
+        </div>
         <button type="submit">Submit</button>
       </form>
     </>
